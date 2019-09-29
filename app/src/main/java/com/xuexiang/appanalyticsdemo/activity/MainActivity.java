a016c32f663c252744b93a9e3e94c5034f880819
xuexiangjys <xuexiangjys@163.com>
2019-09-29 09:43:57 +0800

    增加通知栏权限

--- app/src/main/java/com/xuexiang/appanalyticsdemo/activity/MainActivity.java
+++ app/src/main/java/com/xuexiang/appanalyticsdemo/activity/MainActivity.java
@@ -17,7 +17,16 @@
 
 package com.xuexiang.appanalyticsdemo.activity;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.umeng.message.PushAgent;
 import com.xuexiang.appanalyticsdemo.fragment.MainFragment;
@@ -32,5 +41,81 @@ public class MainActivity extends XPageActivity {
         PushAgent.getInstance(this).onAppStart();
 
         openPage(MainFragment.class);
+
+        if (!isNotifyPermissionOpen(this)) {
+            new AlertDialog.Builder(this)
+                    .setCancelable(false)
+                    .setMessage("通知权限未打开，是否前去打开？")
+                    .setPositiveButton("是", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface d, int w) {
+                            openNotifyPermissionSetting(MainActivity.this);
+                        }
+                    })
+                    .setNegativeButton("否", null)
+                    .show();
+        }
+    }
+
+    /**
+     * 通知栏权限是否获取
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNotifyPermissionOpen(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return NotificationManagerCompat.from(context).getImportance() != NotificationManager.IMPORTANCE_NONE;
+        }
+        return NotificationManagerCompat.from(context).areNotificationsEnabled();
+    }
+
+    /**
+     * 打开通知栏权限设置页面
+     *
+     * @param context
+     */
+    public static void openNotifyPermissionSetting(Context context) {
+        try {
+            Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //直接跳转到应用通知设置的代码：
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, context.getPackageName());
+                intent.putExtra(Settings.EXTRA_CHANNEL_ID, context.getApplicationInfo().uid);
+                context.startActivity(intent);
+                return;
+            }
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+                intent.putExtra("app_package", context.getPackageName());
+                intent.putExtra("app_uid", context.getApplicationInfo().uid);
+                context.startActivity(intent);
+                return;
+            }
+            if (android.os.Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + context.getPackageName()));
+                context.startActivity(intent);
+                return;
+            }
+
+            //4.4以下没有从app跳转到应用通知设置页面的Action，可考虑跳转到应用详情页面,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+                intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                intent.setData(Uri.fromParts("package", context.getPackageName(), null));
+                context.startActivity(intent);
+                return;
+            }
+
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setClassName("com.android.settings", "com.android.setting.InstalledAppDetails");
+            intent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
+            context.startActivity(intent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
